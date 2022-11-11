7a26256b7a05a041fa39a78c7814bda5a22fe01a
Satoshiwoniisama <wolfensonsatoshi@gmail.com>
2022-11-11 16:16:00 -0300

    bot files

---
+++ bot.js
@@ -0,0 +1,88 @@
+const { REST, Routes } = require('discord.js');
+const Discord = require('discord.js');
+const client = new Discord.Client({ 
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMembers,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.MessageContent,
+    Discord.GatewayIntentBits.GuildMessageReactions,
+    Discord.GatewayIntentBits.GuildEmojisAndStickers,
+  ],
+  partials: [Discord.Partials.Message, Discord.Partials.Channel, Discord.Partials.Reaction, Discord.Partials.GuildMember, Discord.Partials.User], 
+});
+
+const dotenv = require("dotenv");
+dotenv.config({path: __dirname + "/.env"});
+
+
+/*########################################*/
+//Server bot
+/*########################################*/
+
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.post("/sendbot",(req, res) => {
+    let message = "";
+    for (const key in req.body) {
+      message += `${key}: ${req.body[key]} \n`;
+    }
+    res.end();
+    rest.post(Routes.channelMessages(channel_id), {
+      body: {
+        content: `${message}`,
+      },
+    }).then(function (res) {
+      console.log(res)
+    });
+});
+
+app.listen(process.env.PORT, process.env.HOST, () => {
+    console.log(`Server running on http://${process.env.HOST}:${process.env.PORT}/`)
+});
+
+/*########################################*/
+//End Server bot
+/*########################################*/
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.TOKEN);
