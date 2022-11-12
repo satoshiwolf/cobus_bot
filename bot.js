3c05b0cf6e5c3d7cff6724dfaca359c9c7cda684
Lucca Polastri <54904556+luccapolastri@users.noreply.github.com>
2022-11-12 01:01:32 -0300

    Update bot.js
    Added the ability to create new channels on post requests.

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const { REST, Routes } = require('discord.js');
 const Discord = require('discord.js');
+const { ChannelType } = require('discord.js');
 const client = new Discord.Client({ 
   intents: [
     Discord.GatewayIntentBits.Guilds,
@@ -9,7 +10,13 @@ const client = new Discord.Client({
     Discord.GatewayIntentBits.GuildMessageReactions,
     Discord.GatewayIntentBits.GuildEmojisAndStickers,
   ],
-  partials: [Discord.Partials.Message, Discord.Partials.Channel, Discord.Partials.Reaction, Discord.Partials.GuildMember, Discord.Partials.User], 
+  partials: [
+    Discord.Partials.Message,
+    Discord.Partials.Channel,
+    Discord.Partials.Reaction,
+    Discord.Partials.GuildMember,
+    Discord.Partials.User,
+  ], 
 });
 
 const dotenv = require("dotenv");
@@ -27,18 +34,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post("/sendbot",(req, res) => {
-  let message = "";
-  for (const key in req.body) {
-    message += `${key}: ${req.body[key]} \n`;
-  }
   res.end();
-  rest.post(Routes.channelMessages(process.env.CHANNEL_ID), {
-    body: {
-      content: `${message}`,
-    },
-  }).then(function (res) {
-    console.log(res)
-  });
+  client.emit('postRequest', req.body);
 });
 
 app.listen(process.env.PORT, process.env.HOST, () => {
@@ -57,6 +54,7 @@ const commands = [
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+let postChannel = null;
 
 (async () => {
   try {
@@ -72,7 +70,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+  postChannel = client.channels.cache.get(process.env.CHANNEL_ID);
 });
 
 client.on('interactionCreate', async interaction => {
@@ -81,6 +79,49 @@ client.on('interactionCreate', async interaction => {
   if (interaction.commandName === 'ping') {
     await interaction.reply('Pong!');
   }
+});
+
+client.on('messageCreate', async message => {
+  if (message.channel.id === process.env.CHANNEL_ID && message.author.id === process.env.CLIENT_ID) {
+    post = JSON.parse(message);
+    channelName = post.channel;
+    message.guild.channels.create({
+      name: channelName,
+      type: ChannelType.GuildText,
+      parent: postChannel.parentId,
+    });
+  }
+});
+
+client.on('channelCreate', async channel => {
+  if(channel.name.startsWith('corbus-ticket')) {
+    post = postChannel.lastMessage;
+    postChannel.lastMessage.delete();
+    post = JSON.parse(post.content)
+    delete post.channel;
+    let message = null;
+    for (const key in post) {
+      message += `${key}: ${post[key]} \n`;
+    }
+    channel.send(message);
+  }
+});
+
+client.on('postRequest', async message => {
+  let channelNames = client.channels.cache.filter(channel =>
+    channel.type == 0 && channel.name.startsWith('cobus-ticket')
+  );
+  channelNames = channelNames.map(channel => channel.name);
+  channelNames.sort();
+  if(channelNames.length <= 0) {
+    channelNames.push("corbus-ticket-0000");
+  }
+  let ticketNum = channelNames.at(-1).split('-');
+  ticketNum[2] = parseInt(ticketNum[2]) + 1;
+  ticketNum[2] = ticketNum[2].toString().padStart(4, '0');
+  ticketNum = ticketNum.join('-');
+  message.channel = ticketNum;
+  postChannel.send(JSON.stringify(message));
 });
 
 client.login(process.env.TOKEN);
